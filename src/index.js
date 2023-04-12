8eb9dd6dfd8a958adf284ca08d08ec7fdea3be8f
BBuehler24 <bbuehler47@yahoo.com>
2023-04-12 16:15:14 -0500

    completed workshop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
 
 const Form = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`The inputted username is: ${username}`)
+    console.log(`The inputted password is: ${password}`)
+    setUsername('')
+    setPassword('')
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+
   return (
-    <div id='container'>
-      <div id='navbar'>
-        Form.js
-      </div>
-      <form>
-        {/* your form fields here */}
+    <div id="container">
+      <div id="navbar">Form.js</div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type='text' name='username' value={username} onChange={handleUsernameChange} />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' value={password} onChange={handlePasswordChange}/>
+        <button type='submit'>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-
-ReactDOM.render(
-  <Form />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Form />, document.getElementById("app"));
